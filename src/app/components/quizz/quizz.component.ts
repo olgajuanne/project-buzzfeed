f3a103afbc82d28da17038f97048c2514b2f86bc
olgajuanne <olgajdango@gmail.com>
2023-01-13 21:39:36 -0300

    estilização

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
   styleUrls: ['./quizz.component.css']
 })
-export class QuizzComponent {
+export class QuizzComponent implements OnInit{
+
+  constructor() {}
+
+  ngOnInit(): void {
+      
+  }
 
 }
